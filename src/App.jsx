10d90cd67159f07367d95eddaa9180a8516e3b26
Sangram Kishor Moharana <42498676+itzSaan@users.noreply.github.com>
2022-10-30 17:14:58 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import MainNav from './sections/MainNav'
 import SocialNav from './sections/SocialNav'
+import MobileNav from './sections/MobileNav'
+import MobileSocialNav from './sections/MobileSocialNav'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Skills from './sections/Skills'
 import Works from './sections/Works'
 import Contact from './sections/Contact'
+
 import './App.css'
 
 function App() {
@@ -14,6 +17,8 @@ function App() {
     <main>
       <MainNav />
       <SocialNav />
+      <MobileNav />
+      <MobileSocialNav />
       <Hero />
       <About />
       <Skills />
