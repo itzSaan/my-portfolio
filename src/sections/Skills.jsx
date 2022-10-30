47321bed7208f7a152f0a2c2b28dee8922d3b864
Sangram Kishor Moharana <42498676+itzSaan@users.noreply.github.com>
2022-10-30 18:04:19 +0530

    Update Skills.jsx

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,4 +1,7 @@
+import { skills } from "../Data";
+
 export default function Skills() {
+    
     return (
         <section id="Skills" className="skills relative h-full md:h-screen">
             <div className="mx-auto py-6 container  h-full grid place-content-center text-center">               
@@ -6,69 +9,12 @@ export default function Skills() {
                 Skills I have<span className="text-rose-600">.</span>
                 </h1> 
               <div className="max-w-5xl lg:my-8 flex flex-wrap justify-center px-10 gap-4">
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex item-center flex-col gap-2">
-                    <img src="images/html_5.svg" alt="" className="block w-full" />
-                    <small>HTML:5</small>
+               {skills.map( skill => (
+                <div key={skill.name} className="w-24 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex flex-col justify-between gap-4 ">
+                    <img src={skill.image} alt="" className="block w-full" />
+                    <small className="block">{skill.name}</small>
                 </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex flex-col gap-2">
-                    <img src="images/css_3.svg" alt="" />
-                    <small>CSS3</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex item-center flex-col gap-2">
-                    <img src="images/js.svg" alt="" />
-                    <small>JavaScript</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex item-center flex-col gap-2">
-                    <img src="images/jquery.svg" alt="" />
-                    <small>jQuery</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex justify-between item-center flex-col gap-2">
-                    <img src="images/bootstrap.svg" alt="" />
-                    <small>Bootstrap</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex justify-between item-center flex-col gap-2">
-                    <img src="images/tailwind.svg" alt="" />
-                    <small>Tailwind CSS</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex item-center flex-col gap-2">
-                    <img src="images/wordpress.svg" alt="" />
-                    <small>WordPress</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex item-center flex-col gap-2">
-                    <img src="images/react.svg" alt="" />
-                    <small>React</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex item-center flex-col gap-2">
-                    <img src="images/node_js.svg" alt="" />
-                    <small>Node JS</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex item-center flex-col gap-2">
-                    <img src="images/vs_code.svg" alt="" />
-                    <small>VS Code</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex item-center flex-col gap-2">
-                    <img src="images/codepen.svg" alt="" />
-                    <small>Codepen</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex item-center flex-col gap-2">
-                    <img src="images/photoshop.svg" alt="" />
-                    <small>Photoshop</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex item-center flex-col gap-2">
-                    <img src="images/coreldraw.svg" alt="" />
-                    <small>CorelDRAW</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl justify-between flex item-center flex-col gap-2">
-                    <img src="images/figma.svg" alt="" />
-                    <small>Figma</small>
-                </div>
-                <div className="w-20 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl justify-between flex item-center flex-col gap-2">
-                    <img src="images/adobexd.svg" alt="" />
-                    <small>Adobe Xd</small>
-                </div>                
-              </div>
-                            
+               ))}               
+              </div>                            
             </div>
         </section>
-    )
-}
