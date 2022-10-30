f831f7f870bc281daddc5ec22f12fd6c55c011b2
Sangram Kishor Moharana <42498676+itzSaan@users.noreply.github.com>
2022-10-30 19:00:01 +0530

    Update Skills.jsx

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -8,13 +8,16 @@ export default function Skills() {
                <h1 className="text-3xl font-bold mb-6 md:mb-10 sm:text-3xl md:text-4xl">
                 Skills I have<span className="text-rose-600">.</span>
                 </h1> 
-              <div className="max-w-5xl lg:my-8 flex flex-wrap justify-center px-10 gap-4">
+              <div className="max-w-5xl lg:my-8 flex flex-wrap justify-center  px-8 gap-4">
                {skills.map( skill => (
-                <div key={skill.name} className="w-24 p-3 sm:w-28 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex flex-col justify-between gap-4 ">
+                <div key={skill.name} className="w-24 p-3 sm:w-28 xl:w-36 rounded-lg hover:bg-slate-800/50 hover:shadow-xl flex flex-col justify-between gap-4 ">
                     <img src={skill.image} alt="" className="block w-full" />
                     <small className="block">{skill.name}</small>
                 </div>
                ))}               
-              </div>                            
+              </div>
+                            
             </div>
         </section>
+    )
+}
